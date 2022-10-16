adcf541515c158fa80ac899f9a82d1389ad73855
DESKTOP-84BFPQT\james <james.wang.w@gmail.com>
2022-10-16 13:36:35 -0400

    refactoring payment.invoice, add bloop

--- ScalaDemo/payment.invoice/src/main/scala/com/aidokay/contract/domain/Lookup.scala
+++ ScalaDemo/payment.invoice/src/main/scala/com/aidokay/contract/domain/Lookup.scala
@@ -3,9 +3,12 @@ package com.aidokay.contract.domain
 import com.aidokay.contract.domain.model.PdfValue
 
 trait Lookup[T] {
-  def value1(implicit data: Map[String, PdfValue], vg: ValueGetter[T]): T = vg.getValue(this, data)
+  def value1(implicit data: Map[String, PdfValue], vg: ValueGetter[T]): Option[T] = vg.getValue(this, data)
   def value[A <: T : ValueGetter](implicit data: Map[String, PdfValue]): A =
-    ValueGetter[A].getValue(this.asInstanceOf[Lookup[A]], data)
+    ValueGetter[A].getValue(this.asInstanceOf[Lookup[A]], data) match {
+      case Some(v) => v
+      case None => ValueGetter[A].identity
+    }
   def getLabel: String
   def getIndex: Int
   def getRange: (Int, Int)
